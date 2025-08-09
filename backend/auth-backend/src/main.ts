4bf4529528b34aae546a7ed7e633740a4a1f1ef7
unknown <nguyennhathoang@gmail.com>
2025-08-09 18:33:56 +0700

    update13

--- backend/auth-backend/src/main.ts
+++ backend/auth-backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://auth-backend-1k2s.onrender.com',
+    origin: 'https://auth-backend-l73o.onrender.com',
     credentials: true,
   });
   await app.listen(3000);
